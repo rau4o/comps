27d81fb913ef1cacdfe3462655a0b37dc0ac463a
rau4o <rauan.k@eventyr.pro>
2024-02-26 00:10:25 +0600

    finished dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -15,13 +15,17 @@ function Dropdown({options, value, onChange}) {
 
     const renderedOptions = options.map((option) => {
         return (
-            <div className="hover:bg-sky-100 rounded cursor-pointer p-1" onClick={() => handleOptionClick(option)} key={option.value}>{option.label}</div>
+            <div className="hover:bg-sky-100 rounded cursor-pointer p-1"
+                 onClick={() => handleOptionClick(option)} key={option.value}>
+                {option.label}
+            </div>
         )
     });
 
     return (
         <div className="w-48 relative">
-            <div className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full" onClick={handleClick}>
+            <div className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+                 onClick={handleClick}>
                 {value?.label || 'Select...'}
                 <GoChevronDown className="text-lg"/>
             </div>
