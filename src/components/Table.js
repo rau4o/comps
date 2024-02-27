6017566991bb45ce2826e893f2bf951f2317cdb0
rau4o <rauan.k@eventyr.pro>
2024-02-27 14:43:48 +0600

    finished sortable table

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,12 +6,12 @@ function Table({data, config, keyFn}) {
         if (column.header) {
             return <Fragment key={column.label}>{column.header()}</Fragment>;
         }
-        return<th key={column.label}>{column.label}</th>
+        return (<th key={column.label}>{column.label}</th>);
     })
 
     const renderedRows = data.map((rowData) => {
         const renderedCells = config.map((column) => {
-            return <td className="p-2" key={column.label}>{column.render(rowData)}</td>
+            return <td className="p-2" key={column?.label}>{column.render(rowData)}</td>
         })
         return <tr className="border-b" key={keyFn(rowData)}>{renderedCells}</tr>
     })
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,4 +1,4 @@
-import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
 
 function TablePage() {
     const data = [
@@ -10,17 +10,24 @@ function TablePage() {
 
     const config = [
         {
-            label: 'fruit',
-            render: (fruit) => fruit.name
+            label: 'Fruit',
+            render: (fruit) => fruit.name,
+            sortValue: (fruit) => fruit.name
         },
         {
-            label: 'color',
-            render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>
+            label: 'Color',
+            render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`} />,
         },
         {
-            label: 'score',
+            label: 'Score',
             render: (fruit) => fruit.score,
-        }
+            sortValue: (fruit) => fruit.score
+        },
+        {
+            label: 'Score Squared',
+            render: (fruit) => fruit.score ** 2,
+            sortValue: (fruit) => fruit.score ** 2
+        },
     ]
 
     const keyFn = (fruit) => {
@@ -29,7 +36,7 @@ function TablePage() {
 
     return (
         <div>
-            <Table data={data} config={config} keyFn={keyFn}/>
+            <SortableTable data={data} config={config} keyFn={keyFn}/>
         </div>
     )
 }
